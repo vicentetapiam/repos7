ff8b6c3a3d7c9e521366841d3662d2bbe251f808
Vicho <vice.tapiam@duocuc.cl>
2025-04-29 10:41:26 -0400

    Primer commit

---
+++ src/main/java/com/seccion7/repos7/Repos7Application.java
@@ -0,0 +1,13 @@
+package com.seccion7.repos7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repos7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repos7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/seccion7/repos7/Repos7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seccion7.repos7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repos7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
